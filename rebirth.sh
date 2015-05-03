31ec4136eb3256dab59e4935dcd93a953b4d5f0b
Alden S. Page <pagea@allegheny.edu>
2015-05-03 04:05:55 -0400

    removed redundant pull

--- rebirth.sh
+++ rebirth.sh
@@ -4,12 +4,7 @@
 # any apt-based distribution.
 # TODO: Redo this in puppet.
 
+# Install packages
 apt-get update
 apt-get install -y git vim python3 ipython3 tmux fakeroot build-essential \
         vagrant virtualbox zsh libncurses5 libncurses5-dev
-
-# Restore configuration files and vim plugins
-cd ~/
-git init
-git remote add origin https://github.com/pagea/dotfiles
-git pull
