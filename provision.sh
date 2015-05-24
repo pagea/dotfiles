06b3d4038d582bbf4654a1ac1b65189de4a31220
Alden S. Page <aldenstpage@gmail.co>
2015-05-24 02:06:12 -0400

    Renamed provisioning script again

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# A script automating the process of setting up a new development environment on
+# any apt-based distribution.
+# TODO: Redo this in puppet.
+
+# Install packages
+apt-get update
+apt-get install -y git vim python3 ipython3 tmux fakeroot build-essential \
+        vagrant virtualbox zsh libncurses5 libncurses5-dev
+# Remove the .git folder so we don't accidentally turn the home folder into a
+# repository
+rm -rf .git
+# Move everything to the home directory
+mv ./.* ./* ~/
